2957d6f958d8cddb8f406d692affb72b7c9ebb61
Jonas Olsson <jonas.olsson1993@gmail.com>
2016-12-28 15:23:21 +0100

    hide all topics with titles containing any of the words

--- redditUtils.js
+++ redditUtils.js
@@ -1,3 +1,15 @@
-function hideAllTopics(titlesArray) {
+const utils = require('./utils');
 
-}
+module.exports = {
+    hideAllTopics(titlesArray) {
+        titlesArray.forEach(title => {
+            const re = new RegExp(title, 'i');
+
+            $('a.title').each(function () {
+                if ($(this).html().match(re)) {
+                    utils.parentN($(this), 3).hide();
+                }
+            });
+        });
+    },
+};
