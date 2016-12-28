8e768024c0746628551a491c4f998c94c00960f8
Jonas Olsson <jonas.olsson1993@gmail.com>
2016-12-28 12:50:17 +0100

    start general things

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 const $ = require('jquery');
+const utils = require('./utils');
+const titleHiders = require('./titleHiders');
 
+const isAt = utils.isAt;
+const parentN = utils.parentN;
+const findParentElement = utils.findParentElement;
+
+// Menu-around actions
+$('a[href="/me/m/w68/"]').attr('href', '/me/m/w68/new/');
+
+if (isAt('w68')) {
+    // expando-button-duplicate
+    setTimeout(function () {
+        $('.expando-button-duplicate').each(function () {
+            parentN($(this), 2).hide();
+        });
+    }, 2000);
+
+    console.log('titleHiders["w68"]: ', titleHiders["w68"]);
+}
