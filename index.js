6f859f1ae2673c6b589bb729f91ab3aea210c57d
Jonas Olsson <jonas.olsson1993@gmail.com>
2016-12-28 16:17:10 +0100

    sidebar, subredditicons, activate some global modules

--- index.js
+++ index.js
@@ -2,17 +2,23 @@ const $ = require('jquery');
 const utils = require('./utils');
 const titleHiders = require('./titleHiders');
 const redditUtils = require('./redditUtils');
+const sidebar = require('./sidebar');
+const subredditIcons = require('./subredditIcons');
 
 const isAt = utils.isAt;
 const parentN = utils.parentN;
 const findParentElement = utils.findParentElement;
 const hideAllTopics = redditUtils.hideAllTopics;
 
-// Menu-around actions
+// Menu-around actions (export to sidenav.js)
 $('a[href="/me/m/w68/"]').attr('href', '/me/m/w68/new/');
 
 // Actions on all pages
 hideAllTopics(titleHiders.all);
+sidebar.initiateToggle();
+redditUtils.hideElements();
+redditUtils.formatTimestamps();
+subredditIcons.initiateIcons();
 
 if (isAt('w68')) {
     // Remove duplicate image links
