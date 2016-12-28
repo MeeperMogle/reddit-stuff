2028123be34e310bc0bf60f4abb883f00d6df8f6
Jonas Olsson <jonas.olsson1993@gmail.com>
2016-12-28 16:15:48 +0100

    framework for adding custom icons after subreddit links

---
+++ subredditIcons.js
@@ -0,0 +1,31 @@
+module.exports = {
+    initiateIcons() {
+        const icons = [
+            {
+                subreddit: 'cynicalbritofficial',
+                class: 'cynicalbrit',
+                iconUrl: 'http://i.imgur.com/RQNQJlp.png',
+            },
+            {
+                subreddit: 'technology',
+                class: 'technologyReddit',
+                iconUrl: 'http://i.imgur.com/5ERrWVv.png',
+            },
+            {
+                subreddit: 'Games',
+                class: 'gamesReddit',
+                iconUrl: 'http://i.imgur.com/qqbE3NA.png',
+            },
+        ];
+
+        let styles = '<style>';
+
+        icons.forEach(icon => {
+            $(`a.subreddit:contains(${icon.subreddit})`).addClass(icon.class);
+
+            styles += `.${icon.class}:after {content:url("${icon.iconUrl}");} `;
+        });
+
+        $('head').append(styles + '</style>')
+    },
+};
