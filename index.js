f164b7c177d83122fc014bc7178c5a87c94209f6
Jonas Olsson <jonas.olsson1993@gmail.com>
2016-12-28 15:23:47 +0100

    redditUtils, hide topics globally and w68

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 const $ = require('jquery');
 const utils = require('./utils');
 const titleHiders = require('./titleHiders');
+const redditUtils = require('./redditUtils');
 
 const isAt = utils.isAt;
 const parentN = utils.parentN;
 const findParentElement = utils.findParentElement;
+const hideAllTopics = redditUtils.hideAllTopics;
 
 // Menu-around actions
 $('a[href="/me/m/w68/"]').attr('href', '/me/m/w68/new/');
 
+// Actions on all pages
+hideAllTopics(titleHiders.all);
+
 if (isAt('w68')) {
-    // expando-button-duplicate
+    // Remove duplicate image links
     setTimeout(function () {
         $('.expando-button-duplicate').each(function () {
             parentN($(this), 2).hide();
         });
     }, 2000);
 
-    console.log('titleHiders["w68"]: ', titleHiders["w68"]);
+    hideAllTopics(titleHiders.w68);
 }
