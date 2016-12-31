c56f04d7020b81623902d5e3708f61a477781d9a
Jonas Olsson <jonas.olsson1993@gmail.com>
2016-12-31 20:42:20 +0100

    shadowbanner persistence in localStorage

--- src/shadowbanner.js
+++ src/shadowbanner.js
@@ -1,12 +1,24 @@
-const shadowbannedNames = [];
+let shadowbannedNames;
+const storageName = 'redditStuffShadowbannerNames';
 
 module.exports = {
     getShadowbannedNames() {
+        shadowbannedNames = JSON.parse(localStorage.getItem(storageName)).names;
+        if (!shadowbannedNames) {
+            console.log('Initiating empty shadow banner list');
+            localStorage.setItem('redditStuffShadowbannerNames', JSON.stringify({
+                names: [],
+            }));
+            shadowbannedNames = JSON.parse(localStorage.getItem(storageName)).names;
+        }
         return shadowbannedNames;
     },
     addShadowbannedName(name) {
         if (!shadowbannedNames.includes(name)) {
             shadowbannedNames.push(name);
+            localStorage.setItem(storageName, JSON.stringify({
+                names: shadowbannedNames,
+            }));
         }
     },
 };
